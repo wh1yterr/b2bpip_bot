aa005d3cd5c49dd4165c5d92fbc9c6e3f6d06a2b
CANNIBAL WH1YTE <90905815+wh1yterr@users.noreply.github.com>
2025-05-17 19:32:46 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Strin, BigInteger
+from sqlalchemy import ForeignKey, String, BigInteger
 from sqlalchemy.orm import Mapped, DeclarativeBase, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
